3a2c652e69238672dc0939c1c841a515e6a77600
Aritra Karan <karanaritra19@gmail.com>
2026-03-08 14:13:59 +0530

    trying to fix issue

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,20 +1,43 @@
-import jwt from "jsonwebtoken"
-const isAuth = async(req,res,next)=>{
-    try {
-        let {token} = req.cookies.token
-        if(!token){
-            return res.status(400).json({message:"user does not have a token"})
-        }
-        const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
+// import jwt from "jsonwebtoken"
+// const isAuth = async(req,res,next)=>{
+//     try {
+//         let token = req.cookies.token
+//         if(!token){
+//             return res.status(400).json({message:"user does not have a token"})
+//         }
+//         const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
 
-        if(!verifyToken){
-            return res.status(400).json({message:"user does not have a token"})
+//         if(!verifyToken){
+//             return res.status(400).json({message:"user does not have a token"})
+//         }
+//         req.userId = verifyToken.userId
+
+//         next()
+//     } catch (error) {
+//         return res.status(500).json({message:`Is auth error ${error}`})
+//     }
+// }
+// export default isAuth;
+import jwt from "jsonwebtoken"
+
+const isAuth = async (req,res,next)=>{
+    try {
+
+        const token = req.cookies.token
+
+        if(!token){
+            return res.status(401).json({message:"Unauthorized"})
         }
-        req.userId = verifyToken.userId
+
+        const decoded = jwt.verify(token , process.env.JWT_SECRET)
+
+        req.userId = decoded.id || decoded.userId
 
         next()
+
     } catch (error) {
-        return res.status(500).json({message:`Is auth error ${error}`})
+        return res.status(401).json({message:`Auth error ${error.message}`})
     }
 }
+
 export default isAuth;
