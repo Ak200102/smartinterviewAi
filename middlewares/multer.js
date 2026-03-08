582477aa8d62a386a49e7d1811f48df31ba71fd8
Aritra Karan <karanaritra19@gmail.com>
2026-03-08 14:25:46 +0530

    issue fixed

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,8 +1,33 @@
+// import multer from "multer"
+
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, "public")
+//     },
+//     filename: function(req , file , cb){
+//         const filename = Date.now() + "-" + file.originalname;
+//         cb(null , filename)
+//     }
+// })
+
+
+// export const upload = multer({
+//     storage,
+//     limits: { fieldSize: 5 * 1024 * 1024},
+// })
 import multer from "multer"
+import fs from "fs"
+
+const uploadPath = "public"
+
+// create folder if it doesn't exist
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true })
+}
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, "public")
+        cb(null, uploadPath)
     },
     filename: function(req , file , cb){
         const filename = Date.now() + "-" + file.originalname;
@@ -10,8 +35,7 @@ const storage = multer.diskStorage({
     }
 })
 
-
 export const upload = multer({
     storage,
-    limits: { fieldSize: 5 * 1024 * 1024},
+    limits: { fileSize: 5 * 1024 * 1024 }  // also fix this
 })
